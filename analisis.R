29fc0a0d2cf8621d686d0d5616b33e09c20dd756
wparedesgt <wparedesgt@gmail.com>
2018-06-19 17:28:39 -0600

    Completando los graficos

--- analisis.R
+++ analisis.R
@@ -1,6 +1,6 @@
 ##Analisis de Datos
 library(tidyverse)
-
+library(ggthemes)
 load("rda/ExitoEscolar.rda")
 
 
@@ -46,7 +46,33 @@ DatosG <- ExitoEscolar %>% filter(edad > 18) %>% select(edad, Sexo)
 CantRegistros <- nrow(DatosG)
 
 
-DatosG %>% ggplot(aes(Sexo, edad)) + geom_boxplot()
+DatosG %>% ggplot(aes(Sexo, edad, fill = Sexo)) + geom_boxplot() + theme_solarized() 
 
 ggsave("plots/boxplot-sexo-edad.png")
+
+
+### Total de Inscritos por Especialidad Grafico Variable Datos
+
+InscritosEspecialidad <- Datos%>% ggplot(aes(Especialidad_Pre_Inscrita, fill = Especialidad_Pre_Inscrita)) + geom_bar() + theme(axis.text = element_text(angle = 45, hjust = 1)) + ylab("Cantidad Total Pre Inscritos") + xlab("Especialidad") 
+
+InscritosEspecialidad
+
+ggsave("plots/Barra-especialidad.png")
+
+
+### Grafico Cantidad de Hombres Vrs Mujeres Inscritas
+
+Genero <- Datos %>% ggplot(aes(Sexo, fill = Sexo)) + geom_bar()+ ylab("Cantidad") + xlab("Genero")
+
+Genero
+
+ggsave("plots/hombres-mujeres-inscritos.png")
+
+### Grafico por Grupo Etnico
+
+PlotGrupoEtnico <- Datos %>% ggplot(aes(Grupo_Etnico, fill = Grupo_Etnico)) + geom_bar() + ylab("Cantidad") + xlab("Grupo Etnico")
+
+PlotGrupoEtnico
+
+ggsave("plots/grupos-etnicos-totales.png")
 
